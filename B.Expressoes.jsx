50ed3c38616c62b2ac2409f2183ba7e918bc66e3
Danilo Santos de Araujo <engenheirodan@outlook.com>
2022-12-08 16:24:26 -0300

    alterando...

---
+++ B.Expressoes.jsx
@@ -0,0 +1,11 @@
+//Expressões são usadas para somar variaveis igual js
+const App = () => {
+    let name: string = 'boniecy';
+    let lastName: string = 'lacerda';
+
+
+return (
+    <div>Soma: {name + lastName}</div>
+);
+}
+export default App;
---
+++ C.varios-componentes.jsx
@@ -0,0 +1,27 @@
+/* Pra trabalhar com varios componentes primeiro cria numa pasta dentro:
+>> componentes
+ > nome do arquivo ex: HEADER
+depois vc importa esse arquivo dentro do APP.tsx
+
+PASTA COMPONENTES > HEADER.TSX
+export const Header = () => {
+    return (
+        <header>
+        <h1> Titulo da página </h1>
+        <hr />
+        </header>
+    );
+}
+*/
+import { Header } from './components/Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+          Ola mundo
+    </div>
+  );
+}
+
+export default App;
